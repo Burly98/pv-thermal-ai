154b7035ede46e45641ef51a810bfc2b1489032f
PV Thermal AI Deployment <deployment@local.invalid>
2026-09-02 12:47:00 +0300

    Refresh Render raster cache keys

--- web/static/map_layers.js
+++ web/static/map_layers.js
@@ -157,7 +157,7 @@
                                 +
                                 "rgb_aligned.jpg"
                                 +
-                                "?v=20260828_render_1"
+                                "?v=20260902_render_ir_2"
                             ),
 
                         projection:
@@ -187,7 +187,7 @@
                                 +
                                 "ir_aligned.jpg"
                                 +
-                                "?v=20260828_render_1"
+                                "?v=20260902_render_ir_2"
                             ),
 
                         projection:
